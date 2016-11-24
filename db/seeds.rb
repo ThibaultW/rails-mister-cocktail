71ed38ce351346c66ff2197ec4b2ec0691688bae
Thibault Willems <thibwillems@scarlet.be>
2016-11-24 12:14:55 +0100

    seed db with ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+list = JSON.parse(open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
+
+list["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
